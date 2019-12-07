3cbf8a35cd836f68fc11d39f79ce5dde9fde35a3
Soumya <soumyas567@gmail.com>
2019-12-07 17:30:44 +0000

    formatted

--- file_exp.py
+++ file_exp.py
@@ -8,13 +8,13 @@ def list_files1(directory, extension):
     return (f for f in listdir(directory) if f.endswith('.' + extension))
 
 
-def fuc(mypath,wcfilter):
+def file_explore(mypath,wcfilter):
     solution = []
     take = []
     print(wcfilter)
     if wcfilter:
-        extesion_filter = (list_files1(mypath, wcfilter))
-        for f in extesion_filter:
+        extension_filter = (list_files1(mypath, wcfilter))
+        for f in extension_filter:
             take.append(f)
     else:
         take = listdir(mypath)
--- file_explorer.py
+++ file_explorer.py
@@ -1,16 +1,21 @@
 import file_exp
 from flask import Flask, render_template, request
-app=Flask(__name__)
+
+app = Flask(__name__)
+
+
 @app.route('/')
 def index():
     return render_template('index.html', )
+
 
 @app.route('/report.html')
 def report():
     path = request.args.get('path')
     wcfilter = request.args.get('wcfilter')
-    list=file_exp.fuc(path,wcfilter)
-    return(render_template('report.html',list=list))
+    list_files = file_exp.file_explore(path, wcfilter)
+    return (render_template('report.html', list=list_files))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
